50b98b33cd726442d43ebe3194694646082fdfec
oliviajanetta <18221056@std.stei.itb.ac.id>
2023-11-22 13:45:13 +0700

    bikin page purchases

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 // import Home from "./Pages/Home";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Payment from "./Pages/Payment";
+import Purchases from "./Pages/Purchases";
 // import Profile from "./Pages/Profile";
 // import Class from "./Pages/Class";
 // import Purchases from "./Pages/Purchases";
@@ -13,9 +14,9 @@ function App() {
         <Routes>
           {/* <Route path="/" element={<Home />} />
           <Route path="/profile" element={<Profile />} /> */}
-          <Route path="/" element={<Payment />} />
-          {/* <Route path="/class" element={<Class />} />
-          <Route path="/purchases" element={<Purchases />} /> */}
+          <Route path="/payment" element={<Payment />} />
+          {/* <Route path="/class" element={<Class />} >*/}
+          <Route path="/purchases" element={<Purchases />} /> 
         </Routes>
       </BrowserRouter>
     </>
